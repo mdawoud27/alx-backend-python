678c85bcc9557cc62ffed71b447f550c34bb9608
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-08-15 22:22:14 +0300

    Task10[Edit]: Duck typing - first element of a sequence {unction docstring}

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,6 +6,15 @@ from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Safely returns the first element of a sequence or None if the sequence is empty.
+
+    Args:
+        lst (Sequence[Any]): A sequence (list, tuple, etc.) of elements of any type.
+
+    Returns:
+        Union[Any, None]: The first element of the sequence if it is not empty, otherwise None.
+    """
     if lst:
         return lst[0]
     else:
