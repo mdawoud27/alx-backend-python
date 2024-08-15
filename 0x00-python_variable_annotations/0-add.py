b5276becdeaf6b965efca7656f7dfdd8f0491a97
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-08-15 17:19:05 +0300

    Task0[Add]: Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+0. Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floating-point numbers.
+
+    Args:
+        a (float): The first number to add.
+        b (float): The second number to add.
+
+    Return:
+        float: The sum of the two input numbers
+    """
+    return a + b
