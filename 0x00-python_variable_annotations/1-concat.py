1a6bb3309954559a70dfdd3dc6d5689ebd491bd0
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-08-15 17:22:38 +0300

    Task1[Add]: Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+1. Basic annotations - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+     Concat two strings together.
+
+     Args:
+         str1 (str): The first string to concat.
+         str2 (str): The second string to concat.
+
+    Return:
+        str: The concat of two strings.
+    """
+    return str1 + str2
