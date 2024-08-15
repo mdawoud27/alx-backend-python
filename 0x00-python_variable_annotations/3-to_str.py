6d5357d37418d7c1077830e3d7f13a42686f15fd
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-08-15 17:24:00 +0300

    Task3[Add]: Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""3. Basic annotations - to string"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert float number to string
+
+    Args:
+         n (float): The float entered number.
+
+    Return:
+        The string representation of the float.
+    """
+    return str(n)
