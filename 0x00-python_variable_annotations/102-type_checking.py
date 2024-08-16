cb64e649d9a68d46c6c7ddb7b08e6603163b42b2
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-08-16 09:52:19 +0300

    Task12[Edit]: Type Checking {fix annotaions}

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -2,10 +2,10 @@
 """
 12. Type Checking
 """
-from typing import Tuple, List, Sequence
+from typing import Tuple, List
 
 
-def zoom_array(lst: Sequence[int], factor: int = 2) -> List[int]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     zoomed_in: List[int] = [
         item for item in lst
         for _ in range(factor)
