b55cb3fd29a5bed61d8f8e0e2713add92266dad0
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-08-21 00:52:50 +0300

    Task1[Add]: Async Comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""1. Async Comprehensions"""
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Coroutine collects 10 random numbers using an async comprehension"""
+    ran_nums = []
+    async for num in async_generator():
+        ran_nums.append(num)
+    return ran_nums
